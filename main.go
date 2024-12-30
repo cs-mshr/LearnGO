8b67c2b0e01c99d2545582093463d2ed59409835
cs-mshr <cshekharmshr2407@gmail.com>
2024-12-31 03:22:21 +0530

    Learn slices(arrays)

--- main.go
+++ main.go
@@ -1,32 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 func main() {
-	//var myOtherMap map[string]string
-	myMap := make(map[string]string)
+	var myslice []string
+	myslice = append(myslice, "hero", "john", "a")
 
-	myMap["Dog"] = "Fido"
-	myMap["Cat"] = "Garfield"
-	myMap["Bird"] = "Tweety"
+	sort.Strings(myslice)
+	log.Println(myslice)
 
-	for key, value := range myMap {
-		println(key, " = ", value)
-	}
-
-	//Integer Map
-	myIntMap := make(map[string]int)
-	log.Println(myIntMap["Dog"]) // default is zero
-
-	myIntMap["Dog"]++
-	log.Println(myIntMap["Dog"]) // zero -> 1
-
-	//Map can have no specified value type also
-	myMapWithoutValueType := make(map[string]interface{})
-	myMapWithoutValueType["Dog"] = "Fido"
-	myMapWithoutValueType["Cat"] = 1
-	myMapWithoutValueType["Bird"] = 7.43
-	for key, value := range myMapWithoutValueType {
-		log.Println(key, " = ", value)
+	numbers := []int{1, 2, 3, 4, 5}
+	sort.Ints(numbers)
+	log.Println(numbers[:4])
+	for val := range numbers {
+		log.Println(val)
 	}
 }
