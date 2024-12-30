21759b8b5be401c9f9230600aab24d2f75b17504
cs-mshr <cshekharmshr2407@gmail.com>
2024-12-31 03:43:02 +0530

    Learn loops and condition

--- main.go
+++ main.go
@@ -3,30 +3,21 @@ package main
 import "log"
 
 func main() {
-	//var myOtherMap map[string]string
-	myMap := make(map[string]string)
+	isTrue := true
 
-	myMap["Dog"] = "Fido"
-	myMap["Cat"] = "Garfield"
-	myMap["Bird"] = "Tweety"
-
-	for key, value := range myMap {
-		println(key, " = ", value)
+	if isTrue {
+		log.Println("Variable is :", isTrue)
+	} else {
+		log.Println("Variable isn't True")
 	}
 
-	//Integer Map
-	myIntMap := make(map[string]int)
-	log.Println(myIntMap["Dog"]) // default is zero
-
-	myIntMap["Dog"]++
-	log.Println(myIntMap["Dog"]) // zero -> 1
-
-	//Map can have no specified value type also
-	myMapWithoutValueType := make(map[string]interface{})
-	myMapWithoutValueType["Dog"] = "Fido"
-	myMapWithoutValueType["Cat"] = 1
-	myMapWithoutValueType["Bird"] = 7.43
-	for key, value := range myMapWithoutValueType {
-		log.Println(key, " = ", value)
+	for i := 0; i < 10; i++ {
+		log.Println(i)
 	}
+
+	animals := []string{"cat", "dog", "fish"}
+	for _, animal := range animals {
+		log.Println(animal)
+	}
+
 }
