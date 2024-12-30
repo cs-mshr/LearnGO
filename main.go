eb6e616611d98386974a0905166436e9f21da997
cs-mshr <cshekharmshr2407@gmail.com>
2024-12-31 03:13:10 +0530

    Learn Maps

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
-import (
-	"log"
-	"time"
-)
-
-type User struct {
-	FirstName string
-	LastName  string
-	Age       int
-	Phone     string
-	BirthDate time.Time
-}
-
-func (m *User) printUserName() string {
-	return m.FirstName + " " + m.LastName
-}
+import "log"
 
 func main() {
+	//var myOtherMap map[string]string
+	myMap := make(map[string]string)
 
-	user := User{
-		FirstName: "John",
-		LastName:  "Doe",
-		Age:       25,
-		Phone:     "123456789",
-		BirthDate: time.Now(),
+	myMap["Dog"] = "Fido"
+	myMap["Cat"] = "Garfield"
+	myMap["Bird"] = "Tweety"
+
+	for key, value := range myMap {
+		println(key, " = ", value)
 	}
 
-	log.Println(user.printUserName())
+	//Integer Map
+	myIntMap := make(map[string]int)
+	log.Println(myIntMap["Dog"]) // default is zero
+
+	myIntMap["Dog"]++
+	log.Println(myIntMap["Dog"]) // zero -> 1
+
+	//Map can have no specified value type also
+	myMapWithoutValueType := make(map[string]interface{})
+	myMapWithoutValueType["Dog"] = "Fido"
+	myMapWithoutValueType["Cat"] = 1
+	myMapWithoutValueType["Bird"] = 7.43
+	for key, value := range myMapWithoutValueType {
+		log.Println(key, " = ", value)
+	}
 }
